f659165f3b459c91c28378c18283efd0e85ac0d3
OzSlapak <ozgeslapak@gmail.com>
2022-09-18 19:28:02 -0400

    js bug equals

--- app.js
+++ app.js
@@ -1,51 +1,86 @@
-const input = document.querySelector('.input');
-const result = document.querySelector('.result');
+const primary = document.querySelector('.primary');
+const secondary = document.querySelector('.secondary');
 const buttons = document.querySelector('.buttons');
-let oper = '';
-let initialNumber = '';
 
-buttons.addEventListener ('click',(e) => {
-    let initialValue = result.innerHTML;
-    let buttonValue = e.target.innerHTML;
-    
-    if(e.target.classList.contains('num')){
-        if(initialValue  === '0' && buttonValue === '0'){
-            console.log('No action');
-        }else if(initialValue === '0'){
-            initialValue.innerHTML = buttonValue;
-        }else {
-            initialValue.innerHTML += buttonValue;
-        }
+buttons.addEventListener('click', (e) => {
+    console.log(e)
+  if (e.target.classList.contains('num')) {
+    if (primary.textContent === '0') {
+      primary.textContent = e.target.innerHTML;
+    } else {
+      primary.textContent =
+        primary.textContent + e.target.innerHTML;
     }
-    if (e.target.classList.contains('oper')) {
-        oper = buttonValue;
-        initialNumber = initialValue;
-        result.innerHTML= `${initialNumber}${oper}`;
-        input.innerHTML = '0';
-        console.log(initialNumber);
+  }
+});
+
+buttons.addEventListener('click', (e) => {
+  if (e.target.classList.contains('oper')) {
+    if (primary.textContent !== '0') {
+      secondary.textContent =
+        primary.textContent + e.target.innerHTML;
+
+      primary.textContent = 0;
     }
-    if (e.target.classList.contains('equals')) {
-        switch(oper) {
-            case '+' :
-                input.innerHTML = 
-                Num(initialNumber) + Num (input.innerHTML);
-                result.innerHTML = '';
-                break;
-            case '-':
-                input.innerHTML = 
-                Num(initialNumber) - Num (input.innerHTML);
-                result.innerHTML = '';
-                break;
-            case 'x ':
-                input.innerHTML = 
-                Num(initialNumber) * Num (input.innerHTML);
-                result.innerHTML = '';
-                break;
-            case '÷':
-                input.innerHTML = 
-                Num(initialNumber) / Num (input.innerHTML);
-                result.innerHTML = '';
-                break;
-        }
+  }
+});
+
+buttons.addEventListener('click', (e) => {
+  if (e.target.classList.contains('pink equals')) {
+    let result = 0;
+    const value1 = Number(secondary.textContent.replace(/[×+−÷]/g, ''));
+    const value2 = Number(primary.textContent);
+    const oper = secondary.textContent.replace(/[0-9.]/g, '');
+    switch (oper) {
+      case '+':
+        result = value1 + value2;
+        break;
+      case '−':
+        result = value1 - value2;
+        break;
+      case '×':
+        result = value1 * value2;
+        break;
+      case '÷':
+        result = value1 / value2;
+        break;
     }
-})
+    secondary.textContent = result;
+    primary.textContent = 0;
+  }
+});
+
+buttons.addEventListener('click', (e) => {
+  if (e.target.classList.contains('clear')) {
+    secondary.textContent = '';
+    primary.textContent = 0;
+  }
+});
+
+buttons.addEventListener('click', (e) => {
+  if (e.target.classList.contains('sign')) {
+    if (!primary.textContent.includes('-')) {
+      primary.textContent = '-' + primary.textContent;
+    } else {
+      primary.textContent = primary.textContent.replace(
+        /[-]/g,
+        ''
+      );
+    }
+  }
+});
+
+buttons.addEventListener('click', (e) => {
+  if (e.target.classList.contains('percent')) {
+    secondary.textContent = primary.textContent / 100;
+    primary.textContent = 0;
+  }
+});
+
+buttons.addEventListener('click', (e) => {
+  if (e.target.classList.contains('decimal')) {
+    if (!primarytextContent.includes('.')) {
+      primary.textContent = primary.textContent + '.';
+    }
+  }
+});
